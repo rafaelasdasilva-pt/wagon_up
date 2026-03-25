7bf998c740ac71ec9111d473cda860a821d36475
Rafaela da Silva <rafaela.s.dasilva@gmail.com>
2026-03-25 20:17:54 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_25_180451) do
   create_table "interviews", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
-    t.text "current_question"
     t.text "feedback_summary"
     t.integer "overall_score"
     t.bigint "role_id", null: false
